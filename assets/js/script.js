65587faea975720fbaf98de9adc13a5a1716472b
tpfk7 <thomas.kerins3@mail.dcu.ie>
2023-02-22 22:22:56 +0000

    Add new buttons to index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,37 @@ let questions = [
 
     },
 ]
+
+const SCORE_POINTS = 100;
+const MAX_QUESTIONS = 4;
+
+function startGame(){
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+function getNewQuestion(){
+    if(availableQuestions.length ===0 || questionCounter > MAX_QUESTIONS){
+        localStorage.setItem('mostRecentScore', score)
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.getElementsByClassName.width =`${(questionCounter/MAX_QUESTIONS) * 100}%`
+
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice =>{
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice'+ number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+}

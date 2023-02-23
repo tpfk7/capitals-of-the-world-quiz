1dff6f6fca4342693b1fc8048b7bc55b463233ba
tpfk7 <thomas.kerins3@mail.dcu.ie>
2023-02-23 23:41:53 +0000

    Add arguments to startGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 const startButton = document.getElementById('start-btn')
+const questionContainerElement =document.getElementById('question-container')
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
-
+    console.log('Started')
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 function setNextQuestion(){
@@ -11,5 +15,5 @@ function setNextQuestion(){
 }
 
 function selectAnswer() {
-    
+
 }

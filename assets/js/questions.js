d355021bba31c911703bca28607ef149bc83cb06
tpfk7 <thomas.kerins3@mail.dcu.ie>
2023-02-24 16:25:30 +0000

    Separate quiz questions from functionality

---
+++ assets/js/questions.js
@@ -0,0 +1,31 @@
+const asiaQuestions = [
+    {
+        question: 'What is the capital of Japan?',
+        answers: [
+            {
+                text: 'Kyoto',
+                correct: false
+            },
+            {
+                text: 'Tokyo',
+                correct: true
+            }
+        ]
+    },
+]
+
+const europeQuestions = [
+    {
+        question: 'What is the capital of Ireland?',
+        answers: [
+            {
+                text: 'Dublin',
+                correct: true
+            },
+            {
+                text: 'Belfast',
+                correct: false
+            }
+        ]
+    },
+]

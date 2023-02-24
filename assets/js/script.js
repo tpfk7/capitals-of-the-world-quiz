84ad7b669bca0f65c3454b5ecd204b1708243b0f
tpfk7 <thomas.kerins3@mail.dcu.ie>
2023-02-24 00:30:21 +0000

    Remove extra html, add functions to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,40 @@
-const startButton = document.getElementById('start-btn')
-const questionContainerElement =document.getElementById('question-container')
+const startButton = document.getElementById('start-btn');
+const questionContainerElement =document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
 
-startButton.addEventListener('click', startGame)
+// Makes questions random
+let shuffledQuestions, currentQuestionsIndex;
+
+startButton.addEventListener('click', startGame);
 
 function startGame() {
-    console.log('Started')
-    startButton.classList.add('hide')
+    console.log('Started');
+    startButton.classList.add('hide');
+    shuffledQuestions = questions.sort(() => Math.random() - 0.5)
+    currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
 }
 
-function setNextQuestion(){
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionsIndex])
+}
 
+function showQuestion(question) {
+    questionElement.innerText = question.question
 }
 
 function selectAnswer() {
 
 }
+
+const questions =[
+    {
+        question: 'What is the capital of Ireland?',
+        answers:[
+            {text: 'Dublin', correct: true},
+            {text: 'Belfast', correct: false}
+        ]
+    },
+]

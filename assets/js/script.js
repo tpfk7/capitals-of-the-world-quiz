ae9d85b97696c96e9b62715754f5362fc811fd5c
tpfk7 <thomas.kerins3@mail.dcu.ie>
2023-02-24 22:03:04 +0000

    Add restart button to quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,7 +76,12 @@ function selectAnswer(e) {
     Array.from(answerButtonsElement.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
     })
+    if (shuffledQuestions.length > currentQuestionIndex +1){
     nextButton.classList.remove('hide')
+    } else{
+        nextButton.innerText = 'Restart'
+        nextButton.classList.remove('hide')
+    }
 }
 
 function setStatusClass(element, correct){

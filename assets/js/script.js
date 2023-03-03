58774c27ebfdcf60b0567f3f45b7529be3617860
tpfk7 <thomas.kerins3@mail.dcu.ie>
2023-03-03 18:29:15 +0000

    Fix timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,147 +1,157 @@
 /* jshint esversion: 11 */
 const continentBtns = document.querySelectorAll('.continent-btn');
-const nextButton = document.getElementById('next-btn')
-const restartButton = document.getElementById('restart-btn')
+const nextButton = document.getElementById('next-btn');
+const restartButton = document.getElementById('restart-btn');
 const questionContainerElement = document.getElementById('question-container');
-const questionElement = document.getElementById('question');
+const country = document.getElementById('country');
 const answerButtonsElement = document.getElementById('answer-buttons');
-const mainTitle = document.getElementById('main-title')
-const startingMinute = 1;
-let time = startingMinute * 60;
+const mainTitle = document.getElementById('main-title');
+let time = 10;
 const countdownEl = document.getElementById('countdown-timer');
 let score = 0;
+let timerInterval;
+const scoreContainer = document.getElementById("score-container");
 
 
 let shuffledQuestions, currentQuestionIndex, questions;
 
 nextButton.addEventListener('click', () => {
-    currentQuestionIndex++
-    setNextQuestion()
-})
+    currentQuestionIndex++;
+    setNextQuestion();
+});
 
 /*Selects the questions by continent*/
 continentBtns.forEach(btn => {
     btn.addEventListener('click', function () {
-        // let continent = btn.dataset.continent;
         let continent = btn.id;
         if (continent == 'europe') {
             questions = europeQuestions;
         } else if (continent == 'asia') {
             questions = asiaQuestions;
-        } else if (continent == 'africa'){
+        } else if (continent == 'africa') {
             questions = africaQuestions;
-        } else if (continent == 'n-america'){
+        } else if (continent == 'n-america') {
             questions = namericaQuestions;
-        } else if (continent == 's-america'){
+        } else if (continent == 's-america') {
             questions = samericaQuestions;
-        } else if (continent == 'oceania'){
+        } else if (continent == 'oceania') {
             questions = oceania;
         }
         startGame();
+
     });
 });
 
-
+/* Once continent is chosen hide continent buttons*/
 
 function startGame() {
-    console.log('Started');
     continentBtns.forEach(btn => {
         btn.classList.add('hide');
     });
+    updateCountdown();
+
 
     /* Makes questions random */
 
     shuffledQuestions = questions.sort(() => Math.random() - 0.5);
     currentQuestionIndex = 0;
     questionContainerElement.classList.remove('hide');
+    scoreContainer.classList.remove('hide');
     setNextQuestion();
-
 }
 
 function setNextQuestion() {
-    resetState()
+    resetState();
     showQuestion(shuffledQuestions[currentQuestionIndex]);
-    
 
 }
+
 
 function showQuestion(question) {
-
-    questionElement.innerText = question.question;
+    country.innerText = question.country;
     question.answers.forEach(answer => {
-        const button =document.createElement('button')
-        button.innerText = answer.text 
-        button.classList.add('btn')
-        if (answer.correct){
-            button.dataset.correct = answer.correct
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        } else {
+            button.dataset.correct = false;
         }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button);
+    });
 }
 
-function resetState(){
-    nextButton.classList.add('hide')
-    while (answerButtonsElement.firstChild){
-        answerButtonsElement.removeChild
-        (answerButtonsElement.firstChild)
+function resetState() {
+    nextButton.classList.add('hide');
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
     }
 }
 
 
 function selectAnswer(e) {
 
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    
-    
-    
-    Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-    if (shuffledQuestions.length > currentQuestionIndex +1){
-    nextButton.classList.remove('hide')
-    } else{
-        restartButton.innerText = 'Restart'
-        restartButton.classList.remove('hide')
-    }
-}
-
-function setStatusClass(element, correct){
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add('correct')
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    if (correct == "true") {
         score++;
-        document.getElementById('score-counter').innerHTML = "Score: " + score;
+        document.getElementById('score-counter').innerText = `${score} out of ${shuffledQuestions.length}`;
 
-    }else {
-        element.classList.add('wrong')
-    
+    }
+
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct);
+    });
+
+    if (shuffledQuestions.length == currentQuestionIndex + 1) {
+        timer = 0;
+        timeOver();
+
+    } else {
+        nextButton.classList.remove('hide');
+    } 
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+
+    if (correct == 'true') {
+        element.classList.add('correct');
+    } else {
+        element.classList.add('wrong');
     }
 }
 
-function clearStatusClass(element){
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
+function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
 }
 
-function scoreCounter(){
-    document.getElementById('score-counter').innerHTML ="Score: " + score;
-
-}
-
-
-scoreCounter();
 
 /* Countdown timer for quiz */
-
-setInterval(updateCountdown, 1000);
-
-function updateCountdown(){
-    const minutes = Math.floor(time/60);
-    let seconds = time % 60;
-
-    countdownEl.innerHTML = `${minutes}: ${seconds}`;
-    time--;
-
+function updateCountdown() {
+    timerInterval = setInterval(() => {
+        countdownEl.innerHTML = time < 10 ? '0' + time : time;
+        if (time <= 0) {
+            timeOver();
+        } else {
+            time--;
+        }
+    }, 1000);
 }
+function timeOver(){
+    clearInterval(timerInterval);
+    questionContainerElement.classList.add('hide');
+    nextButton.classList.add('hide');
+    restartButton.classList.remove('hide');
+}
+
+// // Change page to gameover
+// // The following stackoverflow was used
+// //  https://stackoverflow.com/questions/52627142/how-can-i-automatically-change-a-page-in-javascript
+
+// function changePage(){
+//     window.open(index.html);
+// }

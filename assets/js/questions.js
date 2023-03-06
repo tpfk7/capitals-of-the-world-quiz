2c1d5b26dff17f757f0b2c2120d03a50871c1650
tpfk7 <thomas.kerins3@mail.dcu.ie>
2023-03-06 00:46:57 +0000

    Add questions to Asia questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -4,7 +4,7 @@
 /*Asia*/
 const asiaQuestions = [
     {
-        country: 'What is the capital of Japan?',
+        country: '🇯🇵 Japan',
         answers: [
             {
                 text: 'Kyoto',
@@ -13,9 +13,438 @@ const asiaQuestions = [
             {
                 text: 'Tokyo',
                 correct: true
+            },
+            {
+                text: 'Okinawa',
+                correct: false
+            },
+            {
+                text: 'Toyama',
+                correct: false
             }
         ]
     },
+    {
+        country: '🇮🇩 Indonesia',
+        answers: [
+            {
+                text: 'Medan',
+                correct: false
+            },
+            {
+                text: 'Jakarta',
+                correct: true
+            },
+            {
+                text: 'Batam',
+                correct: false
+            },
+            {
+                text: 'Surabaya',
+                correct: false
+            }
+        ]
+    },
+    {
+        country: '🇰🇷 South Korea',
+        answers: [
+            {
+                text: 'Busan',
+                correct: false
+            },
+            {
+                text: 'Seoul',
+                correct: true
+            },
+            {
+                text: 'Daegu',
+                correct: false
+            },
+            {
+                text: 'Changwon',
+                correct: false
+            }
+        ]
+    },
+    {
+        country: '🇨🇳 China',
+        answers: [
+            {
+                text: 'Beijing',
+                correct: true
+            },
+            {
+                text: 'Shanghai',
+                correct: false
+            },
+            {
+                text: 'Xi An',
+                correct: false
+            },
+            {
+                text: 'Fuzhou',
+                correct: false
+            }
+        ]
+    },
+    {
+    country: '🇮🇳 India',
+    answers: [
+        {
+            text: 'New Delhi',
+            correct: true
+        },
+        {
+            text: 'Mumbai',
+            correct: false
+        },
+        {
+            text: 'Kolkata',
+            correct: false
+        },
+        {
+            text: 'Surat',
+            correct: false
+        }
+    ]
+},
+{
+    country: '🇵🇰 Pakistan',
+    answers: [
+        {
+            text: 'Lahore',
+            correct: false
+        },
+        {
+            text: 'Karachi',
+            correct: false
+        },
+        {
+            text: 'Multan',
+            correct: false
+        },
+        {
+            text: 'Islamabad',
+            correct: true
+        }
+    ]
+},
+{
+    country: '🇧🇩 Bangladesh',
+    answers: [
+        {
+            text: 'Rangpur',
+            correct: false
+        },
+        {
+            text: 'Sylhet',
+            correct: false
+        },
+        {
+            text: 'Cumilla',
+            correct: false
+        },
+        {
+            text: 'Dhaka',
+            correct: true
+        }
+    ]
+},
+{
+    country: 'the 🇵🇭  Philippines',
+    answers: [
+        {
+            text: 'Cebu',
+            correct: false
+        },
+        {
+            text: 'Makati',
+            correct: false
+        },
+        {
+            text: 'Manila',
+            correct: true
+        },
+        {
+            text: 'Baguio',
+            correct: false
+        }
+    ]
+},
+{
+    country: '🇻🇳 Vietnam',
+    answers: [
+        {
+            text: 'Hue',
+            correct: false
+        },
+        {
+            text: 'Dalat',
+            correct: false
+        },
+        {
+            text: 'Hanoi',
+            correct: true
+        },
+        {
+            text: 'Da Nang',
+            correct: false
+        }
+    ]
+},
+{
+    country: '🇹🇷 Turkey',
+    answers: [
+        {
+            text: 'Istanbul',
+            correct: false
+        },
+        {
+            text: 'Ankara',
+            correct: true
+        },
+        {
+            text: 'Konya',
+            correct: false
+        },
+        {
+            text: 'Antalya',
+            correct: false
+        }
+    ]
+},
+{
+    country: '🇮🇷 Iran',
+    answers: [
+        {
+            text: 'Yazd',
+            correct: false
+        },
+        {
+            text: 'Tehran',
+            correct: true
+        },
+        {
+            text: 'Kerman',
+            correct: false
+        },
+        {
+            text: 'Tabriz',
+            correct: false
+        }
+    ]
+},
+{
+    country: '🇹🇭 Thailand',
+    answers: [
+        {
+            text: 'Bangkok',
+            correct: true
+        },
+        {
+            text: 'Phuket',
+            correct: false
+        },
+        {
+            text: 'Chiang Mai',
+            correct: false
+        },
+        {
+            text: 'Udon Thani',
+            correct: false
+        }
+    ]
+},
+{
+    country: '🇹🇭 Myanmar',
+    answers: [
+        {
+            text: 'Naypyidaw',
+            correct: true
+        },
+        {
+            text: 'Yangon',
+            correct: false
+        },
+        {
+            text: 'Bago',
+            correct: false
+        },
+        {
+            text: 'Monywa',
+            correct: false
+        }
+    ]
+},
+{
+    country: '🇮🇶 Iraq',
+    answers: [
+        {
+            text: 'Baghdad',
+            correct: true
+        },
+        {
+            text: 'Mosul',
+            correct: false
+        },
+        {
+            text: 'Karbala',
+            correct: false
+        },
+        {
+            text: 'Erbil',
+            correct: false
+        }
+    ]
+},
+{
+    country: '🇦🇫 Afghanistan',
+    answers: [
+        {
+            text: 'Kabul',
+            correct: true
+        },
+        {
+            text: 'Kandahar',
+            correct: false
+        },
+        {
+            text: 'Herat',
+            correct: false
+        },
+        {
+            text: 'Ghazni',
+            correct: false
+        }
+    ]
+},
+{
+    country: '🇸🇦 Saudi Arabia',
+    answers: [
+        {
+            text: 'Riyadh',
+            correct: true
+        },
+        {
+            text: 'Mecca',
+            correct: false
+        },
+        {
+            text: 'Jeddah',
+            correct: false
+        },
+        {
+            text: 'Tabuk',
+            correct: false
+        }
+    ]
+},
+{
+    country: '🇺🇿 Uzbekistan',
+    answers: [
+        {
+            text: 'Termez',
+            correct: false
+        },
+        {
+            text: 'Bukhara',
+            correct: false
+        },
+        {
+            text: 'Kokand',
+            correct: false
+        },
+        {
+            text: 'Tashkent',
+            correct: true
+        }
+    ]
+},
+{
+    country: '🇲🇾 Malaysia',
+    answers: [
+        {
+            text: 'Malacca',
+            correct: false
+        },
+        {
+            text: 'Sandakan',
+            correct: false
+        },
+        {
+            text: 'Ipoh',
+            correct: false
+        },
+        {
+            text: 'Kuala Lumpur',
+            correct: true
+        }
+    ]
+},
+{
+    country: '🇾🇪 Yemen',
+    answers: [
+        {
+            text: 'Aden',
+            correct: false
+        },
+        {
+            text: 'Taizz',
+            correct: false
+        },
+        {
+            text: 'Ibb',
+            correct: false
+        },
+        {
+            text: 'Sana a',
+            correct: true
+        }
+    ]
+},
+{
+    country: '🇾🇪 Nepal',
+    answers: [
+        {
+            text: 'Pokhara',
+            correct: false
+        },
+        {
+            text: 'Janakpur',
+            correct: false
+        },
+        {
+            text: 'Lalitpur',
+            correct: false
+        },
+        {
+            text: 'Kathmandu',
+            correct: true
+        }
+    ]
+},
+{
+    country: '🇰🇵 North Korea',
+    answers: [
+        {
+            text: 'Wonsan',
+            correct: false
+        },
+        {
+            text: 'Sariwon',
+            correct: false
+        },
+        {
+            text: 'Pyongyang',
+            correct: true
+        },
+        {
+            text: 'Nampo',
+            correct: false
+        }
+    ]
+},
+
 ]
 
 /*Africa*/
